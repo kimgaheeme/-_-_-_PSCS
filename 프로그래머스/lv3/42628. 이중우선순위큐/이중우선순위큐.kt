98877a05018de8548df471c2f6be383f813f9415
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2022-10-05 00:37:56 +0900

    [level 3] Title: 이중우선순위큐, Time: 28.70 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun solution(operations: Array<String>): IntArray {
+        var answer = intArrayOf()
+        
+        operations.forEach {
+            when(it[0]) {
+                'I' -> {
+                    answer = answer.plus(it.substringAfter("I ").toInt())
+                }
+                else -> {
+                    if(it == "D 1") answer = deleteMax(answer)
+                    else answer = deleteMin(answer)
+                }
+            }
+        }
+        return intArrayOf(answer.maxOrNull()?: 0,answer.minOrNull()?: 0)
+    }
+}
+
+fun deleteMax(arr:IntArray): IntArray = if (arr.size == 1) intArrayOf()
+    else { 
+        val maxValue = arr.maxOrNull()
+        arr.filter {  x -> x != maxValue }.toIntArray()}
+
+fun deleteMin(arr:IntArray): IntArray = if (arr.size == 1) intArrayOf()
+    else {
+         val minValue = arr.maxOrNull()
+         arr.filter {  x -> x != arr.minOrNull() }.toIntArray()
+    }
