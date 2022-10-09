73b1dad0a8b65d2020ae2bc583d8137063007509
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2022-10-09 17:52:57 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.kt
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.kt
@@ -1,46 +1,34 @@
 class Solution {
     fun solution(gems: Array<String>): IntArray {
-        
-        //맨 앞에 위치하는 값
+        val mp = hashMapOf<String, Int>()
+        gems.forEach { mp[it] = 0 }
         var first = 0
-        var count = 0
-        
-        //제일 작은 길이 값
-        var start: Int = 0
-        var end: Int = 0
-        var len = gems.size + 1
-        
-        //queue에 담겨진 각각 숫자
-        var numCount1 = gems.toSet()
-        var numCount = mutableMapOf<String, Int>()
-        
-        if(numCount.size == gems.size) return intArrayOf(1, gems.size)
-        
-        numCount1.forEach {
-            numCount[it] = 0
-        }
-       
-        var i = 0
-        
-        while(i < gems.size) {
-            //만약 이 값이 처음이라면 count를 늘린다.
-            if(numCount[gems[i]]!! == 0) count++
-            numCount.put(gems[i],numCount[gems[i]]!!+1)
-            
-            //첫번째 값이 it과 같고 queue에 2개 이상이 들어있으면 뺀다.
-            while(numCount[gems[first]]!! > 1) {
-                numCount.put(gems[first],numCount[gems[first]]!!-1)
-                first++
-            }
-            
-            i++
-            if(count == numCount.size && i - first < len ) {
-                len = i - first
-                start = first
-                end = i
+        var second = 9999999
+
+        val full = mp.size
+
+        var left = 0
+        var right = mp.size - 1
+        for (i in 0 until mp.size)
+            mp[gems[i]] = mp[gems[i]]!! + 1
+        var contains = mp.count { it.value != 0 }
+        while (right <= gems.size) {
+            if (contains == full) {
+                if (second - first > right - left) {
+                    second = right
+                    first = left
+                }
+                if (mp[gems[left]] == 1) contains--
+                mp[gems[left]] = mp[gems[left]]!! - 1
+                left++
+            } else {
+                right++
+                if (right < gems.size) {
+                    if (mp[gems[right]] == 0) contains++
+                    mp[gems[right]] = mp[gems[right]]!! + 1
+                }
             }
         }
-        
-        return intArrayOf(start+1, end)
+        return intArrayOf(first + 1, second + 1)
     }
 }
