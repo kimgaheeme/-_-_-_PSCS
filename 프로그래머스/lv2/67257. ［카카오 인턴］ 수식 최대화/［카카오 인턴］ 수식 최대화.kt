b24fda408e496ed412c4a1d5c2e5e158748e427b
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2022-11-07 23:37:33 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 31.11 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.kt
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.kt
@@ -5,7 +5,7 @@ class Solution {
         var answer: Long = 0
         var arr = arrayListOf<String>()
         var temp = ""
-    
+        var candidate = listOf("*+-","*-+","+*-","+-*","-+*","-*+")
         
         expression.forEach {
             if(it in listOf('+','-','*')) {
@@ -18,32 +18,25 @@ class Solution {
         }
         arr.add(temp)
         
-        var t: Long = abs(getArr(arrayListOf("*","+","-"), arr, 0))
-        if(t > answer) answer = t
-        t = abs(getArr(arrayListOf("*","-","+"), arr, 0))
-        if(t > answer) answer = t
-        t = abs(getArr(arrayListOf("+","-","*"), arr, 0))
-        if(t > answer) answer = t
-        t = abs(getArr(arrayListOf("+","*","-"), arr, 0))
-        if(t > answer) answer = t
-        t = abs(getArr(arrayListOf("-","*","+"), arr, 0))
-        if(t > answer) answer = t
-        t = abs(getArr(arrayListOf("-","+","*"), arr, 0))
-        if(t > answer) answer = t
+        candidate.forEach {
+            var t: Long = abs(getArr(it, arr, 0))
+            if(t > answer) answer = t
+        }
+        
         
         
         return answer
     }
 }
 
-fun getArr(opArr: ArrayList<String>, arr: ArrayList<String>, depth: Int): Long {
-    if(depth == opArr.size) {
+fun getArr(opArr: String, arr: ArrayList<String>, depth: Int): Long {
+    if(depth == opArr.length) {
         return arr[0].toLong()
     }else{
         var newArr = arrayListOf<String>()
         var index = 0
         while(index < arr.size) {
-            if(arr[index] == opArr[depth]) {
+            if(arr[index] == opArr[depth].toString()) {
                 newArr[newArr.size - 1] = getResult(opArr[depth], newArr[newArr.size - 1].toLong(), arr[index + 1].toLong() ).toString()
                 index++
             }
@@ -56,10 +49,10 @@ fun getArr(opArr: ArrayList<String>, arr: ArrayList<String>, depth: Int): Long {
     }
 }
 
-fun getResult(op: String, num1: Long, num2: Long): Long {
+fun getResult(op: Char, num1: Long, num2: Long): Long {
     when(op) {
-        "+" -> return num1 + num2
-        "-" -> return num1 - num2
+        '+' -> return num1 + num2
+        '-' -> return num1 - num2
         else -> return num1 * num2
     }
 }
