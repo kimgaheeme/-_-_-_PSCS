634cf71b496a34db24db4fa7a9ec3909e6e8ed2f
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-03-03 10:24:52 +0900

    [level 1] Title: 폰켓몬, Time: 4.12 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,12 @@
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        HashMap<Integer, Integer> poketmon = new HashMap<>();
-        int i = 0;
-        int len = nums.length;
-        
-        //각 포켓몬의 개수를 구한다.
-        for(i = 0; i < len; i++) {
-            poketmon.put(nums[i], poketmon.containsKey(nums[i]) ? (poketmon.get(nums[i]) + 1) : 1);
-        }
-        
-        //만약 포켓몬 종류 >= N/2 이면 정답은 N/2 
-        //그렇지 않으면 포켓몬 종류이다.
-        if(poketmon.size() >= len / 2) return len / 2;
-        else return poketmon.size();
+        return Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.collectingAndThen(Collectors.toSet(), 
+                                                        poketmon -> Integer.min(nums.length / 2 , poketmon.size())));
     }
 }
+    
