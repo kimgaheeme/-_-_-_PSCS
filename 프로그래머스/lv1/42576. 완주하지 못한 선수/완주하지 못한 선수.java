3eccb7ea5656ed6720941c92c3c5881b5b2bcf9b
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-03-03 10:59:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.69 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -8,11 +8,11 @@ class Solution {
         
         HashMap<String, Integer> people = new HashMap<String, Integer>();
         
-        Arrays.stream(participant)
-            .forEach(p -> {people.put(p, people.containsKey(p) ? people.get(p) + 1 : 1);});
-        
-        Arrays.stream(completion)
-            .forEach(p -> {if(people.get(p).equals(1)) people.remove(p); else people.put(p, people.get(p) - 1);});
+        for(String p : participant) people.put(p, people.containsKey(p) ? people.get(p) + 1 : 1);
+        for(String p : completion) {
+            if(people.get(p).equals(1)) people.remove(p); 
+            else people.put(p, people.get(p) - 1);
+        }
         
         for(String s : people.keySet()) answer = s;
         
