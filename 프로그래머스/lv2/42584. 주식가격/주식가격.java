bd682e8b7acf597c118ef33d03deabd8225fd82b
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-03-30 22:32:52 +0900

    [level 2] Title: 주식가격, Time: 27.45 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,15 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
-        int i, j;
-        for (i = 0; i < len; i++) {
-            for (j = i + 1; j < len; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j])
-                    break;
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> list = new Stack<>();
+        list.push(0);
+
+        int i = 1;
+
+        for(i = 1; i < prices.length; i++) {
+            while(!list.isEmpty() && prices[list.peek()] > prices[i]){
+                    answer[list.peek()] = i - list.peek();
+                    list.pop();
             }
+            list.push(i);
         }
+
+        while(!list.isEmpty()){
+            answer[list.peek()] = i - list.peek() - 1;
+            list.pop();
+        }
+
         return answer;
     }
 }
