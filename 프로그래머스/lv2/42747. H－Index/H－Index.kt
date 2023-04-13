6a497944ada396d268ecba59291a1e79f8ffabeb
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 13:01:19 +0900

    [level 2] Title: H-Index, Time: 14.28 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.kt
+++ 프로그래머스/lv2/42747. H－Index/H－Index.kt
@@ -1,14 +1,6 @@
 class Solution {
     fun solution(citations: IntArray): Int {
         var answer = 0
-        
-        citations.sortDescending()
-    
-        
-        for(i in citations.size downTo 1) {
-            if(citations[i - 1] >= i) return i
-        }
-        
-        return 0
+        return answer
     }
 }
