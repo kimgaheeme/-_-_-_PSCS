25c97eb41e8669b4d580dddc5243aa874416cf04
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 09:54:37 +0900

    [level 2] Title: 기능개발, Time: 13.46 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.kt
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.kt
@@ -1,25 +1,31 @@
+import java.util.*
 import kotlin.math.*
 
 class Solution {
     fun solution(progresses: IntArray, speeds: IntArray): IntArray {
         var answer = intArrayOf()
+        var stack = Stack<Int>()
+        var now = ceil((100 - progresses[0]) / speeds[0].toDouble()).toInt()
+        var count = 1
         
-        var front = -1
-        var count = 0
-        var days = progresses
-            .mapIndexed{ index, it -> ceil((100 - it) / speeds[index].toDouble()).toInt()}
-            .forEach{
-                if(front == -1) front = it
-                
-                if(front >= it) count++
-                else if(front < it) {
-                    front = it
-                    answer += count
-                    count = 1
-                }
+        for(i in progresses.size - 1 downTo 1) {
+            stack.push(ceil((100 - progresses[i]) / speeds[i].toDouble()).toInt())
+        }
+        
+        while(!stack.isEmpty()) {
+            if(now >= stack.peek()) {
+                count++
+            } else {
+                now = stack.peek()
+                answer += count
+                count = 1 
             }
+            stack.pop()
+        }
         
         answer += count
+        
+        
         return answer
     }
 }
