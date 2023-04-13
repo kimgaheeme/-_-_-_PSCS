9b492cc8bba1cdf6cc25054abbc340cb524634e8
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 10:57:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.45 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.kt
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.kt
@@ -2,61 +2,45 @@ import java.util.*
 import kotlin.math.*
 
 class Solution {
-    
-    var answer: Int = 100
-    var link = mutableMapOf<Int, MutableList<Int>>()
-    
     fun solution(n: Int, wires: Array<IntArray>): Int {
+        var answer: Int = Int.MAX_VALUE
+        var m = mutableMapOf<Int, MutableList<Int>>()
         
-        //map만들기
         wires.forEach {
-            var list1 = link.getOrDefault(it[0], mutableListOf<Int>())
-            var list2 = link.getOrDefault(it[1], mutableListOf<Int>())
+            var list1 = m.getOrDefault(it[0],mutableListOf<Int>())
+            var list2 = m.getOrDefault(it[1],mutableListOf<Int>())
             list1.add(it[1])
             list2.add(it[0])
             
-            link.put(it[0], list1)
-            link.put(it[1], list2)
-
+            m.put(it[0], list1)
+            m.put(it[1], list2)
         }
         
-        println(link)
-        
-        wires.forEach { getDiff(it, n) }
+        wires.forEach {
+            var num = getNum(m, n, it[0], it[1])
+            if(abs(n - num - num) < answer) answer = abs(n - num - num)
+        }
         
         return answer
     }
     
-    fun getDiff(wire: IntArray, n: Int) {
+    fun getNum(wires: MutableMap<Int, MutableList<Int>>, size: Int, start: Int, end: Int): Int {
         var queue = LinkedList<Int>()
-        var visited = BooleanArray(n){ false }
-        queue.add(1)
-        visited[0] = true
+        var visited = BooleanArray(size + 1){ false }
         var count = 0
-        
+        queue.add(1)
         
         while(queue.size != 0) {
-            var now = queue[0]
+            var now = queue.poll()
+            visited[now] = true
             count++
-            queue.removeFirst()
             
-            link.getOrDefault(now, mutableListOf<Int>()).forEach {
-                if(!visited[it - 1] && !(now == wire[0] && it == wire[1]) && !(now == wire[1] && it == wire[0])) {
-                    //println("$now $it  ${wire[0]}  ${wire[1]}")
-                    queue.add(it)
-                    visited[it - 1] = true
-                }
+            wires.getOrDefault(now, mutableListOf<Int>()).forEach {
+                if(!visited[it] &&
+                        (now != start || it != end) && (it != start || now != end)) queue.add(it)
             }
-            
         }
         
-        
-        
-        var diff = n - count
-        
-        if(abs(diff - count) < answer) {
-            answer = abs(diff - count)
-            //println("$diff  $count")
-        }
+        return count
     }
 }
