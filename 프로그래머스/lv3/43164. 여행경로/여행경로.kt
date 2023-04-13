a192086f704a19632ab8f974c2d8d18e5a69c52c
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 11:24:18 +0900

    [level 3] Title: 여행경로, Time: 10.74 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.kt
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.kt
@@ -1,61 +1,43 @@
 class Solution {
     
-    var answer = arrayOf<Array<String>>()
-    var ticketSize = 0
-    lateinit var visited: BooleanArray
-    lateinit var ticketArray: Array<Array<String>>
+    var answerList = mutableListOf<Array<String>>()
+    lateinit var used: BooleanArray
     
     fun solution(tickets: Array<Array<String>>): Array<String> {
+        var answer = arrayOf<String>()
+        used = BooleanArray(tickets.size) { false }
         
-        ticketArray = tickets
-        ticketSize = tickets.size
-        visited = BooleanArray(ticketSize){ false }
+        var m = mutableMapOf<String, MutableList<Int>>()
         
-        //map으로 연결되어있는지 표시
-        var ticket = mutableMapOf<String, MutableList<Int>>()
+        tickets.sortBy{it[1]}
         tickets.forEachIndexed { index, it ->
-            var list = ticket.getOrDefault(it[0], mutableListOf<Int>())
+            var list = m.getOrDefault(it[0], mutableListOf<Int>())
             list.add(index)
-            ticket.put(it[0], list)
+            m.put(it[0], list)
         }
         
-        //count로 dfs
-        dfs(arrayOf<String>("ICN"), "ICN", ticket)
-    
-        return getAnswer()
+        dfs(arrayOf<String>("ICN"), m, tickets.size + 1, "ICN", tickets)
+        
+        
+        return answerList[0]
     }
-    fun dfs(selected: Array<String>, now: String, ticket: MutableMap<String, MutableList<Int>>) {
-        //만약 selected가 tickets개수와 같다면 return
-        if(selected.size == ticketSize + 1) {
-            answer += selected 
-        }else{
-            //now에서 출발할 수 있는 비행권 순회 하는데 ticket에서 빼기
-            var list = ticket.getOrDefault(now, mutableListOf<Int>())
-            
-            list.forEach{  it ->
-                if(!visited[it]) {
-                    visited[it] = true
-                    var dest = ticketArray[it][1]
-                    dfs(selected + dest, dest, ticket)
-                    visited[it] = false
+    
+    fun dfs(select: Array<String>, ticket: MutableMap<String, MutableList<Int>>, size: Int, now: String, tickets: Array<Array<String>>) {
+        
+        
+        if(select.size == size) {
+            answerList.add(select)
+            return 
+        }
+        
+        else {
+            ticket.getOrDefault(now, mutableListOf<Int>()).forEach {
+                if(!used[it]) {
+                    used[it] = true
+                    dfs(select + tickets[it][1],ticket, size, tickets[it][1], tickets)
+                    used[it] = false
                 }
             }
         }
-        
-    }
-    
-    fun getAnswer(): Array<String> {
-        var list = answer.map{
-            it.reduce(){
-                total, s -> total + s
-            }
-        }.sorted()[0]
-        
-        var answer = arrayOf<String>()
-        
-        repeat(ticketSize + 1) {
-            answer += list.substring(it * 3, it * 3 + 3)
-        }
-        return answer
     }
 }
