1013ecbc5cf961ca4c98e3b41574e34e6be5ede9
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 12:42:38 +0900

    [level 1] Title: 최소직사각형, Time: 25.11 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 78 MB, 시간: 45.51 ms
+메모리: 73.6 MB, 시간: 25.11 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.kt
@@ -1,23 +1,19 @@
-import kotlin.math.*;
+import kotlin.math.*
 
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-        var pair = Pair(sizes[0][0], sizes[0][1])
-        var answer: Int = pair.first * pair.second
+        var answer: Int = 0
+        var minNum = 0
+        var maxNum = 0
         
         sizes.forEach {
-            var sum1 = listOf(pair.first, it[0]).maxOrNull()!! * listOf(pair.second, it[1]).maxOrNull()!!
-            var sum2 = listOf(pair.first, it[1]).maxOrNull()!! * listOf(pair.second, it[0]).maxOrNull()!!
+            var mi = it.minOrNull()!!
+            var ma = it.maxOrNull()!!
             
-            if( sum1 > sum2) {
-                pair = Pair(listOf(pair.first, it[1]).maxOrNull()!!, listOf(pair.second, it[0]).maxOrNull()!!)
-                answer = sum2
-            } else {
-                pair = Pair(listOf(pair.first, it[0]).maxOrNull()!!, listOf(pair.second, it[1]).maxOrNull()!!)
-                answer = sum1
-            }
+            if(minNum < mi) minNum = mi
+            if(maxNum < ma) maxNum = ma
         }
         
-        return answer
+        return minNum * maxNum
     }
 }
