d1ceb67398d9d3c5525ea3ec64d65f639c1a4f19
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 14:53:36 +0900

    [level 2] Title: 모음 사전, Time: 18.90 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/README.md
+++ 프로그래머스/lv2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 64.2 MB, 시간: 31.42 ms
+메모리: 63.8 MB, 시간: 18.90 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.kt
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.kt
@@ -1,26 +1,26 @@
 class Solution {
     
-    var dict = mutableSetOf<String>()
+    var dict = mutableListOf<String>()
     
     fun solution(word: String): Int {
         var answer = 0
-        var chatList = listOf("A", "E", "I", "O", "U")
+        var c = listOf('A', 'E', 'I', 'O', 'U')
         
-        dfs("", chatList)
-        dict.toList().sorted().forEachIndexed { index, it ->
-            if(it == word) return index + 1  
+        dfs("", c)
+        
+        dict.forEachIndexed { index, it ->
+            if(it == word) return index + 1
         }
-        
-        
         return answer
     }
-    fun dfs(selected: String, charList: List<String>) {
-        if(selected.length != 0) dict.add(selected)
+    
+    fun dfs(selected: String, c: List<Char>) {
+        if(selected != "") dict.add(selected)
         if(selected.length == 5) return
-        
         repeat(5) {
-            dfs(selected + charList[it], charList)
+            dfs(selected + c[it], c)
         }
-        
     }
+    
+    
 }
