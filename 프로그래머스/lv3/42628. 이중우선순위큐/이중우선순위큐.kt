30ccb19b51fb9d1b9d959ab4b01e06a4f946cb29
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 18:11:22 +0900

    [level 3] Title: 이중우선순위큐, Time: 11.16 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.kt
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.kt
@@ -1,45 +1,60 @@
 import java.util.*
-
 class Solution {
     fun solution(operations: Array<String>): IntArray {
         var answer = intArrayOf(0, 0)
-        var map = mutableMapOf<Int, Int>()
-        var minHeap = PriorityQueue<Int>()
-        var maxHeap = PriorityQueue<Int>(Comparator{ a, b -> if( a < b) 1 else -1 })
-        var size = 0
-
-        operations.forEach { it ->
-            var list = it.split(" ")
-            
-            when(list[0]) {
-                "I" -> {
-                    map.put(list[1].toInt(), map.getOrDefault(list[1].toInt(), 0) + 1)
-                    minHeap.add(list[1].toInt())
-                    maxHeap.add(list[1].toInt())
-                    size++
+        var m = mutableMapOf<Int, Int>()
+        var count = 0
+        
+        var pqMin = PriorityQueue<Int>()
+        var pqMax = PriorityQueue<Int>(Comparator { a, b -> b - a})
+        
+        operations.forEach {
+            var op = it.split(" ")
+            if(op[0] == "I") {
+                var num = op[1].toInt()
+                var c = m.getOrDefault(num, 0)
+                m.put(num, c + 1)
+                count++
+                pqMin.add(num)
+                pqMax.add(num)
+            }else if(op[1] == "-1") {
+                if(count != 0) {
+                    while(m.getOrDefault(pqMin.peek(), 0) == 0) {
+                        pqMin.poll()
+                    }
+                    var newNum = m.getOrDefault(pqMin.peek(), 0) - 1
+                    m.put(pqMin.peek(), newNum)
+                    pqMin.poll()
+                    count--
                 }
                 
-                "D" -> {
-                    if(list[1] == "1" && size != 0) {
-                        while(map[maxHeap.peek()]!! == 0) { maxHeap.poll() }
-                        map.put(maxHeap.peek(), map[maxHeap.peek()]!! - 1)
-                        size--
-                    } else if(list[1] == "-1" && size != 0) {
-                        while(map[minHeap.peek()]!! == 0) { minHeap.poll() }
-                        map.put(minHeap.peek(), map[minHeap.peek()]!! - 1)
-                        size--
+            } else {
+                if(count != 0) {
+                    while(m.getOrDefault(pqMax.peek(), 0) == 0) {
+                        pqMax.poll()
                     }
+                    var newNum = m.getOrDefault(pqMax.peek(), 0) - 1
+                    m.put(pqMax.peek(), newNum)
+                    pqMax.poll()
+                    count--
+                    
                 }
             }
         }
         
-        if(size == 0) return intArrayOf(0, 0)
-        else {
-            while(map[maxHeap.peek()]!! == 0) { maxHeap.poll() }
-            while(map[minHeap.peek()]!! == 0) { minHeap.poll() }
-            return intArrayOf(maxHeap.peek(), minHeap.peek())
+        if(count == 0) return answer
+        
+        while(m.getOrDefault(pqMax.peek(), 0) == 0) {
+            pqMax.poll()
         }
+        while(m.getOrDefault(pqMin.peek(), 0) == 0) {
+            pqMin.poll()
+        }
+        
+        answer[0] = pqMax.peek()
+        answer[1] = pqMin.peek()
         
         return answer
     }
 }
+
