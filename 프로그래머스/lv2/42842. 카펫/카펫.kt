9d29feed8ea3a08ee048c33cccd85934616a98cd
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 10:18:54 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.kt
+++ 프로그래머스/lv2/42842. 카펫/카펫.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun solution(brown: Int, yellow: Int): IntArray {
         var answer = intArrayOf()
-        var apb = (brown - 4) /2
         
-        for( b in 1 .. 2000000) {
-            if(yellow % b == 0) {
-                var a = yellow / b
-                
-                if(a + b == apb) return intArrayOf(a + 2,b + 2)
+        for(a in 1 until brown) {
+            if(yellow % a == 0) {
+                var b = yellow / a
+                if((2 * (a + b) + 4) == brown) {
+                    return intArrayOf(b + 2, a + 2)
+                }
             }
             
         }
