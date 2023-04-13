3ed8739ab923a6faf8efbd2f48be029378f2f6b4
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-13 10:11:02 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.16 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -1,35 +1,31 @@
-import java.util.*;
+import java.util.*
 
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        
         var answer = 0
-        var queue = LinkedList<Int>(List(bridge_length){0})
-        var totalWeight = 0
+        var s = 0
+        var count = 0
         var now = 0
-        var done = 0
+        var queue = LinkedList<Int>()
+        repeat(bridge_length) { queue.add(0) }
         
-        while(done < truck_weights.size) {
+        while(count != truck_weights.size) {
             answer++
-            
-            var front = queue[0]
+            if(queue.peek() != 0) {
+                count++
+                if(count == truck_weights.size) break;
+            }
+            s -= queue.peek()
             queue.removeFirst()
-            totalWeight -= front
             
-            if(front != 0){
-                done++
-                if(done == truck_weights.size) break;
+            if(now < truck_weights.size ) {
+                if(s + truck_weights[now] <= weight) {
+                    s += truck_weights[now]
+                    queue.add(truck_weights[now])
+                    now++
+                }else { queue.add(0) }
             }
             
-            var truck = if(now < truck_weights.size) truck_weights[now] else 0
-            
-            if(totalWeight + truck <= weight) {
-                totalWeight += truck
-                queue.add(truck)
-                if(now < truck_weights.size) now++
-            }else {
-                queue.add(0)
-            }
         }
         
         return answer
