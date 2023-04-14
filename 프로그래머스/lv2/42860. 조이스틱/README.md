01e26df32b89df1176bcc41d2c27b28bff51303e
kimgaheeme <77616888+kimgaheeme@users.noreply.github.com>
2023-04-14 16:09:42 +0900

    [level 2] Title: 조이스틱, Time: 16.77 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/README.md
+++ 프로그래머스/lv2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=kotlin#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 63.7 MB, 시간: 15.76 ms
+메모리: 63.6 MB, 시간: 16.77 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.kt
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.kt
@@ -1,60 +1,65 @@
+import java.util.*
 import kotlin.math.*
 
 class Solution {
     
+    var targetCount = 0
     lateinit var visited: BooleanArray
-    var min = 500
-    var s = ""
+    var min = Int.MAX_VALUE
     
     fun solution(name: String): Int {
-        s = name
-        var list = mutableListOf<Int>()
-        if(name[0] == 'A') list.add(0)
+        visited = BooleanArray(name.length){ false }
         name.forEachIndexed { index, it ->
-            if(it != 'A') list.add(index)
+            if(it != 'A') {
+                targetCount++
+            }else {
+                visited[index] = true
+            }
         }
-        visited = BooleanArray(list.size){ false }
         visited[0] = true
         
-        dfs(1, list, 0, 0)
+        dfs(if(name[0] != 'A') 1 else 0, 0, getVerti(name[0]), name)
+        
         
         return min
     }
     
-    fun dfs(selected: Int, list: MutableList<Int>, count: Int, start: Int) {
-        var total = count +  getCount(s[start])
-        //println("selected: $selected  start: $start total: $total")
+    fun dfs(depth: Int, start: Int, selected: Int, name: String) {
         
-        if(selected == list.size) {
-            if(min > total) min = total
-            return
+        
+        if(depth == targetCount) {
+            if(min > selected) min = selected
+            return 
         }
         
-        else {
-            repeat(list.size) {
-                if(!visited[it]) {
-                    visited[it] = true
-                    dfs(selected + 1, list, total + getLength(start, list[it]), list[it])
-                    visited[it] = false
-                }
+        repeat(name.length) {
+            if(!visited[it]) {
+                visited[it] = true
+                dfs(depth + 1, it, selected + getCount(name[it], start, it, name.length), name)
+                visited[it] = false
             }
         }
+        
     }
     
-    fun getLength(st: Int, e: Int): Int {
-        var start = listOf(st, e).minOrNull()!!
-        var end = listOf(st, e).maxOrNull()!!
+    
+    fun getHori(start: Int, end: Int, length: Int): Int {
+        var min = listOf(start, end).minOrNull()!!
+        var max = listOf(start, end).maxOrNull()!!
         
-        var left = abs(start + s.length - end)
-        var right = abs(end - start)
+        var left = max - min
+        var right = length + min - max
         
-        if(right > left) return left
-        else return right
+        return listOf(left, right).minOrNull()!!
     }
     
-    fun getCount(c: Char): Int {
-        var num = c - 'A'
-        if(num > (26 - num)) return 26 - num
-        else return num
+    fun getVerti(c: Char): Int {
+        var v = c - 'A'
+        
+        return listOf(v, 26 - v).minOrNull()!!
+    }
+    
+    fun getCount(c: Char, start: Int, end: Int, length: Int): Int {
+        return getVerti(c) + getHori(start, end, length)
     }
 }
